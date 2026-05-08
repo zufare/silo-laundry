f5736841835c932f350c6c5751771d3a8dc73331
Rielva Zufar <zufare123@gmail.com>
2026-05-08 17:08:04 +0700

    add interactive laundry form

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("laundryForm");
+const tableBody = document.getElementById("laundryTableBody");
+
+form.addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  const name =
+    document.getElementById("customerName").value;
+
+  const weight =
+    document.getElementById("weight").value;
+
+  const service =
+    document.getElementById("service").value;
+
+  const row = `
+    <tr>
+      <td>${name}</td>
+      <td>${weight} kg</td>
+      <td>${service}</td>
+      <td>
+        <span class="status process">
+          Diproses
+        </span>
+      </td>
+    </tr>
+  `;
+
+  tableBody.innerHTML += row;
+
+  form.reset();
+
+});
